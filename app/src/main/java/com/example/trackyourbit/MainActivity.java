07ecc1b4e0f03f9ece26f5e60a0e5e86fc1ec770
umang <iumangsharma001@gmail.com>
2020-05-19 00:08:02 +0530

    FULL APP WORKING

--- app/src/main/java/com/example/trackyourbit/MainActivity.java
+++ app/src/main/java/com/example/trackyourbit/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.trackyourbit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,17 +14,28 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Currency;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public String BitURL = "g";
-    public String APPId = "z";
+    public String APPId = "MjMyMjk4NjVmZGVjNGUyMDljOTA3YzM5ZTJkYWU3OTQ";
 
 
-    public String Currency;
-    public int loc;
+    public String mSymbolSet = "global";
+    public String mperiod = "hour";
+    public String mCurrency;
+    public int mLoc;
     TextView mRateTextView;
     Spinner mSpinner;
 
@@ -31,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRateTextView = findViewById(R.id.rateTextView);
-        mSpinner= findViewById(R.id.spinner);
+        mSpinner = findViewById(R.id.spinner);
 
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.currency_array, android.R.layout.simple_spinner_item);
@@ -41,28 +55,60 @@ public class MainActivity extends AppCompatActivity {
         mSpinner.setAdapter(adapter);
 
 
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view1, int pos, long id)
-            {
-                loc = pos;
-                Currency = (String) parent.getItemAtPosition(loc);
-                Log.d("Bitcoin","Currency Selected is  "+ Currency);
+            public void onItemSelected(AdapterView<?> parent, View view1, int pos, long id) {
+                mLoc = pos;
+                mCurrency = (String) parent.getItemAtPosition(mLoc);
+                Log.d("Bitcoin", "Currency Selected is  " + mCurrency);
 
+                someNetworking(mCurrency);
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> arg1)
-            {
-                Log.d("Bitcoin","Nothing S0");
+            public void onNothingSelected(AdapterView<?> arg1) {
+                Log.d("Bitcoin", "Nothing S0");
+
+            }
+
+
+        });
+    }
+
+    public void someNetworking(String cur) {
+
+        String mURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"+cur;
+
+        Log.d("Bitcoin",mURL);
+
+        AsyncHttpClient mclient = new AsyncHttpClient();
+
+        mclient.get(mURL, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("Bitcoin", "DATA COLLECTED" + response.toString());
+
+                try {
+                    String price = response.getString("last");
+                    mRateTextView.setText(price);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                Log.e("Bitcoin", "Fail  " + e.toString());
+                Log.d("Bitcoin", "Fail..status code" + statusCode);
 
             }
         });
     }
 
 
-
-
-
 }
+
+
+
+
+
