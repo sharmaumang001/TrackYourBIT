0e3e8df50035940cfa802c4e9f6316bb35084f93
umang <iumangsharma001@gmail.com>
2020-05-18 14:11:51 +0530

    Spinner added and linked.

--- app/src/main/java/com/example/trackyourbit/MainActivity.java
+++ app/src/main/java/com/example/trackyourbit/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.trackyourbit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mRateTextView;
+    Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRateTextView = findViewById(R.id.rateTextView);
+        mSpinner= findViewById(R.id.spinner);
+
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.currency_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mSpinner.setAdapter(adapter);
     }
 }
