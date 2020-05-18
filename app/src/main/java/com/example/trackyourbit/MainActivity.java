ea951117bd277c5368d8f46977595fa48ef88503
umang <iumangsharma001@gmail.com>
2020-05-18 16:00:49 +0530

    Spinner object parsed,

--- app/src/main/java/com/example/trackyourbit/MainActivity.java
+++ app/src/main/java/com/example/trackyourbit/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.trackyourbit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Currency;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    public String BitURL = "g";
+    public String APPId = "z";
+
+
+    public String Currency;
+    public int loc;
     TextView mRateTextView;
     Spinner mSpinner;
 
@@ -26,5 +39,30 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         mSpinner.setAdapter(adapter);
+
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view1, int pos, long id)
+            {
+                loc = pos;
+                Currency = (String) parent.getItemAtPosition(loc);
+                Log.d("Bitcoin","Currency Selected is  "+ Currency);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg1)
+            {
+                Log.d("Bitcoin","Nothing S0");
+
+            }
+        });
     }
+
+
+
+
+
 }
